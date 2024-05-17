c4ccd4cc9bb8338fc0af0818ab4ecc1b36a84d91
Tolibjon0601 <egamberdiyevtolibjon0601@gmail.com>
2024-05-17 22:04:01 +0500

    start

---
+++ main.js
@@ -0,0 +1,52 @@
+// let obj = {
+//  a: 1,
+//  b: 2,
+//  c: 3,
+//  d: 1
+// };
+
+// let newObj= {};
+// for (let key in obj) {
+//  if (!Object.values(newObj).includes(obj[key])) {
+//   newObj[key] = obj[key];
+//  }
+// }
+
+// console.log(newObj);
+
+// 2-masala
+// let obj=[9,8,44,7,23,4,2,101];
+// obj.sort((a,b)=>a-b);
+// console.log(obj);
+
+
+
+// uyga vazifa
+
+// 1-misol
+// Given an array of ints length 3, return the sum of all the elements.
+// function sum3(nums){
+//  return nums[0]+nums[1]+nums[2]
+//  }
+
+// 2-misol
+// m va n butun sonlari berilgan m sonini n marta chiqaruvchi dastur tuzilsin
+// let m=Number(prompt(" m sonini kiriting: "))
+// let n=Number(prompt("n sonini kiritng: "))
+// for( let i=0;i<n;i++){
+//  console.log(m);
+// }
+// 3-misol
+
+// a dan b gacha bo'lgan butun sonlar yig'indisi topilsin
+
+// let a=3;
+// let b=20;
+// let sum=0;
+// for( let i=a;i<=b;i++){
+//  sum+=i;
+// }
+// console.log(sum);
+// 4-misol
+// n soni berilgan n sonini tub yoki tub emasligini aniqlovchi dastur tuzilsin
+let n=Number(prompt("n sonini kiriting: "));
